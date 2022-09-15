ddb9f294f1d1df89923a771ed57b4276ccfff4d2
Michael Engher <enghermichael@gmail.com>
2022-09-15 17:47:26 +0300

    completed basic functionality + 2 optimizations

--- reactjs/create-react-app-root/01-create-react-app/monsters-rolodex/src/App.js
+++ reactjs/create-react-app-root/01-create-react-app/monsters-rolodex/src/App.js
@@ -2,8 +2,52 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = event => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
-    return <div className="App"></div>;
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map(monster => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
   }
 }
 
